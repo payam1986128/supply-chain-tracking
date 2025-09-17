80fc3d6a33b613895290a1a417d999ee8c783d71
Mostafaei Payam <p-mostafaie@gamelectronics.com>
2025-09-17 14:54:57 +0330

    initial commit

---
+++ src/main/java/ir/greenweb/examples/supplychaintracking/SupplyChainTrackingApplication.java
@@ -0,0 +1,13 @@
+package ir.greenweb.examples.supplychaintracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyChainTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplyChainTrackingApplication.class, args);
+    }
+
+}
