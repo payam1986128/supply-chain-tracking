8e9ab442d08991b6bcaa3fd7c68e8001390ecfb8
Mostafaei Payam <p-mostafaie@gamelectronics.com>
2025-09-19 15:07:05 +0330

    configure context path

--- presentation/src/main/java/ir/greenweb/examples/supplychaintracking/presentation/rest/ProductController.java
+++ presentation/src/main/java/ir/greenweb/examples/supplychaintracking/presentation/rest/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/products")
 @AllArgsConstructor
 public class ProductController {
 
